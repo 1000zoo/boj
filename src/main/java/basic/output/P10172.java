3e304e47c93e5a0d0e956421d808e178c54c0041
1000zoo <cjswldn99@gmail.com>
2023-12-26 18:01:15 +0900

    백준 / 새싹 / 출력 / 개

---
+++ src/main/java/basic/output/P10172.java
@@ -0,0 +1,16 @@
+//2023-12-26
+//https://www.acmicpc.net/problem/10172
+//개
+
+package basic.output;
+
+public class P10172 {
+
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n"
+                + "|q p|   /}\n"
+                + "( 0 )\"\"\"\\\n"
+                + "|\"^\"`    |\n"
+                + "||_/=\\\\__|");
+    }
+}
