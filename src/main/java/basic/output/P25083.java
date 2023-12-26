fee7c91694843cf79cd39141f24d7e07524a75a5
1000zoo <cjswldn99@gmail.com>
2023-12-26 18:02:59 +0900

    백준 / 새싹 / 출력 / 새싹

---
+++ src/main/java/basic/output/P25083.java
@@ -0,0 +1,16 @@
+//2023-12-26
+//https://www.acmicpc.net/problem/25083
+//새싹
+
+package basic.output;
+
+public class P25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n"
+                + "r`-_   ,'  ,/\n"
+                + " \\. \". L_r'\n"
+                + "   `~\\/\n"
+                + "      |\n"
+                + "      |");
+    }
+}
