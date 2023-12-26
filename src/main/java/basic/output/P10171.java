e19e049c17c8660efc5212a830fa3b684476fc53
1000zoo <cjswldn99@gmail.com>
2023-12-26 17:53:05 +0900

    백준 / 새싹 / 출력 / 고양이

---
+++ src/main/java/basic/output/P10171.java
@@ -0,0 +1,17 @@
+//2023-12-26
+//https://www.acmicpc.net/source/70763764
+//고양이
+
+package basic.output;
+
+public class P10171 {
+
+    public static void main(String[] args) {
+        String cat = "\\    /\\\n"
+                + " )  ( ')\n"
+                + "(  /  )\n"
+                + " \\(__)|";
+
+        System.out.println(cat);
+    }
+}
