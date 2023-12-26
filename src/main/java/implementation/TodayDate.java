ab0b359dc3505c4eed6b78ae7640709cbb6c70ef
1000zoo <cjswldn99@gmail.com>
2023-12-26 17:38:44 +0900

    백준 / 오늘날짜

---
+++ src/main/java/implementation/TodayDate.java
@@ -0,0 +1,15 @@
+//2023-12-26
+//https://www.acmicpc.net/source/70762925
+//오늘 날짜
+
+package implementation;
+
+import java.time.LocalDate;
+
+public class TodayDate {
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+}
