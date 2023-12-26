6a88ffee75846e4dbb902b3f493e4be77e457ad8
1000zoo <cjswldn99@gmail.com>
2023-12-26 17:49:27 +0900

    백준 / 새싹 / 출력 / 오늘 날짜

---
+++ src/main/java/basic/output/P07287.java
@@ -0,0 +1,11 @@
+//2023-12-26
+//
+
+package basic.output;
+
+public class P07287 {
+    public static void main(String[] args) {
+        System.out.println(3);
+        System.out.println("cjswldn99");
+    }
+}
